d60b2faae33f4616441e688bee2aab3cc4d30f24
kk <112804222+kar-ik@users.noreply.github.com>
2025-04-01 17:54:06 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,8 +1,13 @@
 import scrapy
+import sys
+from scrapy.crawler import CrawlerProcess
 
 class WebCrawler(scrapy.Spider):
     name = "web_crawler"
-    start_urls = ["http://example.com"] 
+
+    def __init__(self, target_url=None, *args, **kwargs):
+        super(WebCrawler, self).__init__(*args, **kwargs)
+        self.start_urls = [target_url] if target_url else ["http://example.com"]
 
     def parse(self, response):
         links = response.xpath("//a/@href").getall()
@@ -27,3 +32,9 @@ class WebCrawler(scrapy.Spider):
         print("\n--- Hidden Elements Found ---")
         for hidden in hidden_elements:
             print(f"Hidden field: {hidden.xpath('@name').get()}")
+
+if __name__ == "__main__":
+    target_url = sys.argv[1] if len(sys.argv) > 1 else "http://example.com"
+    process = CrawlerProcess()
+    process.crawl(WebCrawler, target_url=target_url)
+    process.start()
